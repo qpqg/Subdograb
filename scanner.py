6cafb052432fc954166bf07b97abcc0360436201
QiubyZ <41333888+QiubyZ@users.noreply.github.com>
2020-05-27 12:01:32 +0800

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,5 +1,6 @@
 from requests import session
 from os import path
+from sys import stdout
 from datetime import datetime
 from pathlib import Path as folder
 q = session()
@@ -19,14 +20,18 @@ def files_save(myfile, text):
 	with open(real_path(myfile), "+a") as files:
 		files.writelines(f"{text}\n")
 def main(domain):
-        banner()
-	folder(real_path(f"/logs/{domain}/")).mkdir(parents=True, exist_ok=True)
-	for host in search(domain).split("\n"):
-		myhost = host.split(",")
-		files_save(f"/logs/{domain}/{domain}_host.txt", myhost[0])
-		files_save(f'/logs/{domain}/{domain}_ip.txt', myhost[1])
-		logs("{} > {}\r\n\r\n".format(myhost[0], myhost[1]))
-
+	banner()
+	mydo = search(domain)
+	if not mydo.startswith("error"):
+		folder(real_path(f"/logs/{domain}/")).mkdir(parents=True, exist_ok=True)
+		for host in mydo.split("\n"):
+			myhost = host.split(",")
+			files_save(f"/logs/{domain}/{domain}_host.txt", myhost[0])
+			files_save(f'/logs/{domain}/{domain}_ip.txt', myhost[1])
+			logs(f"{myhost[0]} > {myhost[1]}\r\n\r\n")
+	else:
+		logs(f"{mydo}\r\n\r\n")
+		
 if __name__ == "__main__":
 	main(input("Host: "))
 #	main("facebook.com")
